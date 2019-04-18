171a015d4d6131f390e9a6183b849f1f104b14e0
Stella McEwen <mcewen.stella@gmail.com>
2019-04-18 18:38:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -42,3 +42,20 @@ def reformat_languages(languages)
      new_hash
   end
 end
+
+
+
+
+
+
+
+new_hash = {}
+languages.each do |style, info|
+    info.each do |language, lang_attributes|
+        if new_hash[language]
+            new_hash[language][:style] << style
+        else
+            new_hash[language] = {type: lang_attributes[:type], style: [style]}
+        end
+    end
+end
