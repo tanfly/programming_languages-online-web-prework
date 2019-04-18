36f942d873ba5335b5ec0867e1408e4b3b44a082
Stella McEwen <mcewen.stella@gmail.com>
2019-04-18 18:17:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,45 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, info|
+    info.each do |language, lang_attributes|
+      lang_attributes[:style] = []
+          new_hash[language] = lang_attributes
+          if new_hash.keys.include? (language)
+            new_hash[language][:style] << style
+        end
+      end
+     new_hash
+  end
 end
